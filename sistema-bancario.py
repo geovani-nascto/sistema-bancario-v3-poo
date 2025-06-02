a5bdf185fce47585e8a780a25ed0165329cabd2a
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2025-06-02 17:52:56 -0300

    att class definition

---
+++ sistema-bancario.py
@@ -0,0 +1,21 @@
+class Conta:
+    pass
+
+class ContaCorrente(Conta):
+    pass
+
+class Cliente:
+    pass
+
+class PessoaFisica(Cliente):
+    pass
+
+class Transacao():
+    pass
+
+class Deposito:
+    pass
+
+class Saque:
+    pass
+
